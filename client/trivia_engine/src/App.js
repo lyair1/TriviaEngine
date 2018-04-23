7ce551a922659d4b0b34622ec623057d95aea86a
Yair Levi <yairle@microsoft.com>
2018-04-23 13:22:56 -0700

    add simple web view with react-js

--- client/trivia_engine/src/App.js
+++ client/trivia_engine/src/App.js
@@ -2,15 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      in_question: '',
+      in_answer_1: '',
+      in_answer_2: '',
+      in_answer_3: '',
+    }
+  }
+
+  isSubmitEnabled() {
+    return this.state.in_question.length > 0 && this.state.in_answer_1.length > 0 && this.state.in_answer_2.length > 0 && this.state.in_answer_3.length > 0
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Trivia Engine</h1>
-        </header>
-        <p className="App-intro">
-          Get ready
-        </p>
+        <h1 className="App-Title">The Trivia Engine</h1>
+        <div className="App-Form-Container">
+          {/* Questions */}
+          <input type="text" value={this.state.in_question} placeholder={"Enter Question"} className="App-Input-Question" onChange={(e) => this.setState({in_question: e.target.value})}/>
+          
+          {/* Answers */}
+          <input type="text" value={this.state.in_answer_1} placeholder={"Answer #1"} className="App-Input-Answer" onChange={(e) => this.setState({in_answer_1: e.target.value})}/>
+          <input type="text" value={this.state.in_answer_2} placeholder={"Answer #2"} className="App-Input-Answer" onChange={(e) => this.setState({in_answer_2: e.target.value})}/>
+          <input type="text" value={this.state.in_answer_3} placeholder={"Answer #3"} className="App-Input-Answer" onChange={(e) => this.setState({in_answer_3: e.target.value})}/>
+
+          <button disabled={!this.isSubmitEnabled()} className={this.isSubmitEnabled() ? "App-Button" : "App-Button-Disabled"}>Find The Answer</button>
+        </div>
+
+        <div>
+          <a href="https://github.com/lyair1/TriviaEngine" > <i className="fab fa-github App-Github"></i></a>
+        </div>
       </div>
     );
   }
